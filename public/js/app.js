65eb8185d49414199a7d54131abdc50151d22203
jasonjun1221 <jasonchong1221@hotmail.com>
2023-02-24 02:50:33 +0800

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    messageOne.textContent = `Loading...`
+    messageTwo.textContent = ``
+
+    // Local VS
+    // http://localhost:3000/weather?address=${search.value}
+
+    // Codespace VS
+    // https://jasonjun1221-opulent-space-giggle-4p67wgw97693qpjq-3000.preview.app.github.dev/weather?address=${search.value}
+
+    fetch(`http://localhost:3000/weather?address=${search.value}`).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            messageOne.textContent = data.error
+        } else {
+            messageOne.textContent = `Location: ${data.location}`
+            messageTwo.textContent = `Forecast: ${data.forecast}`
+        }
+    })
+})
+})
+
+// fetch('https://puzzle.mead.io/puzzle',).then((response) => {
+//     response.json().then((data) => {
+//         console.log(data);
+//     })
+// })
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longtitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=d88ada21a49babe1d5cba8967d3b6fe4&query=${latitude},${longtitude}&units=f`
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect the weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to search location!', undefined)
+        } else {
+            callback(undefined, `${body.current.weather_descriptions[0]}, It is ${body.current.temperature} degress out. It is likely ${body.current.feelslike}% chance to rain.`)
+        }
+    })
+}
+
+module.exports = forecast
